1fb78529ecc02db2c1c32da925a0cedc7b7212c2
Alex Svoboda <svoboda7760@gmail.com>
2021-04-21 20:51:39 -0500

    Add MetaMask fix

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,5 +6,11 @@ require("@nomiclabs/hardhat-waffle");
 require("./tasks/faucet");
 
 module.exports = {
-  solidity: "0.7.3"
+  solidity: "0.7.3",
+
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+  }
 };
